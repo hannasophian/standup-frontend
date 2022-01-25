52421ee156ee129d681810cc394004bf20be7388
Hanna <sophianhanna@gmail.com>
2022-01-25 17:44:50 +0000

    Edit/add notes modal

--- src/components/NotesDisplay.tsx
+++ src/components/NotesDisplay.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Modal from "react-modal";
 import toStringDate from "../utils/helpers/toStringDate";
 import StandupInterface from "../utils/interfaces/StandupInterface";
+import "../css/standupcard.css"
 
 interface NotesDisplayProps {
   currentUserID: number;
@@ -10,26 +11,29 @@ interface NotesDisplayProps {
 }
 
 export default function NotesDisplay(props: NotesDisplayProps): JSX.Element {
-  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+  const [notesInput, setNotesInput] = useState<string>();
+  const [seenotesModalIsOpen, setseeNotesModalIsOpen] = useState<boolean>(false);
+  const [addModalIsOpen, setAddModalIsOpen] = useState<boolean>(false);
+
   // if there are notes, have show notes button
   const notesButton = () => {
-    return <button onClick={() => setModalIsOpen(true)}>Show Notes</button>;
+    return <button onClick={() => setseeNotesModalIsOpen(true)}>Show Notes</button>;
   };
   // If no notes and user = chair, have 'add notes' button
   const addNoteButton = () => {
-    return <button>Add Notes</button>;
+    return <button onClick={() => setAddModalIsOpen(true)}>Add Notes</button>;
   };
 
   useEffect(() => {
     console.log(props.standup);
   }, [props.standup]);
 
-  const modal = () => {
+  const seeNotesModal = () => {
     return (
       <Modal
         id="see-notes-modal"
-        isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
+        isOpen={seenotesModalIsOpen}
+        onRequestClose={() => setseeNotesModalIsOpen(false)}
         contentLabel="Notes Modal"
         ariaHideApp={false}
       >
@@ -42,13 +46,56 @@ export default function NotesDisplay(props: NotesDisplayProps): JSX.Element {
           <button
             type="button"
             className="close"
-            onClick={() => setModalIsOpen(false)}
+            onClick={() => setseeNotesModalIsOpen(false)}
           >
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div className="modal-body">
+          {props.chairID===props.currentUserID && <button
+            type="button"
+            className="edit"
+            onClick={() => {setseeNotesModalIsOpen(false); setAddModalIsOpen(true)}}
+          > Edit
+            <span aria-hidden="true">&times;</span>
+          </button>}
           <p>{props.standup.notes}</p>
+        </div>
+      </Modal>
+    );
+  };
+
+  const addNotesModal = () => {
+    return (
+      <Modal
+        id="add-notes-modal"
+        isOpen={addModalIsOpen}
+        onRequestClose={() => setAddModalIsOpen(false)}
+        contentLabel="Add Notes Modal"
+        ariaHideApp={false}
+      >
+        <div className="modal-header">
+          <div className="content">
+            <h2>Add/Update Notes</h2>
+            <p>{toStringDate(props.standup.time)}</p>
+            <p>Chair: {props.standup.chair_name}</p>
+          </div>
+          <button
+            type="button"
+            className="close"
+            onClick={() => setAddModalIsOpen(false)}
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div className="modal-body">
+          <textarea
+            id="notes-input"
+            rows={7}
+            placeholder="Add notes to this standup. What did you do? Are there any common problems? Were any questions raised? "
+            onChange={(e) => setNotesInput(e.target.value)}>{props.standup.notes !== null && props.standup.notes}</textarea>
+          <br />
+          <button onClick={() => {setAddModalIsOpen(false);console.log(notesInput)}}>OK</button>
         </div>
       </Modal>
     );
@@ -61,7 +108,8 @@ export default function NotesDisplay(props: NotesDisplayProps): JSX.Element {
         props.currentUserID === props.chairID &&
         addNoteButton()}
 
-      {props.standup.notes !== null && modal()}
+      {props.standup.notes !== null && seeNotesModal()}
+      {addNotesModal()}
     </div>
   );
 }
