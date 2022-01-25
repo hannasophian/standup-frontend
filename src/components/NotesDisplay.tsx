b2e56568e92437ac874fd3274f0069e4efe796b0
Hanna <sophianhanna@gmail.com>
2022-01-25 17:01:48 +0000

    Show existing notes when button is clicked

--- src/components/NotesDisplay.tsx
+++ src/components/NotesDisplay.tsx
@@ -1,4 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Modal from "react-modal";
+import toStringDate from "../utils/helpers/toStringDate";
 import StandupInterface from "../utils/interfaces/StandupInterface";
 
 interface NotesDisplayProps {
@@ -8,9 +10,10 @@ interface NotesDisplayProps {
 }
 
 export default function NotesDisplay(props: NotesDisplayProps): JSX.Element {
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
   // if there are notes, have show notes button
   const notesButton = () => {
-    return <button>Show Notes</button>;
+    return <button onClick={() => setModalIsOpen(true)}>Show Notes</button>;
   };
   // If no notes and user = chair, have 'add notes' button
   const addNoteButton = () => {
@@ -21,12 +24,44 @@ export default function NotesDisplay(props: NotesDisplayProps): JSX.Element {
     console.log(props.standup);
   }, [props.standup]);
 
+  const modal = () => {
+    return (
+      <Modal
+        id="see-notes-modal"
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        contentLabel="Notes Modal"
+        ariaHideApp={false}
+      >
+        <div className="modal-header">
+          <div className="content">
+            <h2>Notes</h2>
+            <p>{toStringDate(props.standup.time)}</p>
+            <p>Chair: {props.standup.chair_name}</p>
+          </div>
+          <button
+            type="button"
+            className="close"
+            onClick={() => setModalIsOpen(false)}
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div className="modal-body">
+          <p>{props.standup.notes}</p>
+        </div>
+      </Modal>
+    );
+  };
+
   return (
     <div className="notesdisplay">
       {props.standup.notes !== null && notesButton()}
       {props.standup.notes === null &&
         props.currentUserID === props.chairID &&
         addNoteButton()}
+
+      {props.standup.notes !== null && modal()}
     </div>
   );
 }
---
+++ src/components/global.d.ts
@@ -0,0 +1,1 @@
+declare module "react-module";
