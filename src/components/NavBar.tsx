18303839a210bae09b09172b10834a8037f426e2
Hanna <sophianhanna@gmail.com>
2022-01-25 05:42:22 +0000

    Initial form - need to add upload function, date input, number input

--- src/components/NavBar.tsx
+++
@@ -1,9 +0,0 @@
-import PageProps from "../utils/interfaces/PageProps";
-
-export default function NavBar(props: PageProps): JSX.Element {
-  return (
-    <div className="navbar">
-      <p>Hi, {props.currentUser.name}</p>
-    </div>
-  );
-}
--- src/utils/interfaces/PageProps.tsx
+++
@@ -1,9 +0,0 @@
-import { UserInterface } from "./UserInterface";
-
-export default interface PageProps {
-  team: number;
-  setTeam: React.Dispatch<React.SetStateAction<number>>;
-
-  currentUser: UserInterface;
-  setCurrentUser: React.Dispatch<React.SetStateAction<UserInterface>>;
-}
