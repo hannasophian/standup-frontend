e2535f307e2f0e2d83c2a991ffc0120e85d72343
Hanna <sophianhanna@gmail.com>
2022-01-26 10:45:53 +0000

    Activity display

--- src/components/ActivitiesDisplay.tsx
+++ src/components/ActivitiesDisplay.tsx
@@ -11,29 +11,44 @@ export default function ActivitiesDisplay(
   props: ActivitiesDisplayProps
 ): JSX.Element {
   const activitylist = props.activities.map((activity) => (
-    <div key={activity.id} className="activity">
-      <p key={activity.id}>{activity.name}</p>
-      <div>
-        {activity.url && (
-          <a href={activity.url}>
-            <p>Go to activity</p>
-          </a>
-        )}
+    <>
+      <div key={activity.id} className="row">
+        <p key={activity.id} className="col">
+          {activity.name}
+        </p>
+        <div className="col">
+          {activity.url && (
+            <a href={activity.url}>
+              <button>Go to Activity</button>
+            </a>
+          )}
+        </div>
       </div>
-      <div>{activity.comment && <p>"{activity.comment}"</p>}</div>
-    </div>
+      <div className="row">
+        {" "}
+        <div className="col">
+          {activity.comment && <p>"{activity.comment}"</p>}
+        </div>
+        <div className="col">
+          {props.currentUserID === props.chairID && (
+            <button className="span">Edit activity</button>
+          )}
+        </div>
+      </div>
+    </>
   ));
   return (
     <>
       <div className="activity-header">
-        <>
-          {(props.currentUserID === props.chairID ||
-            props.activities.length !== 0) && <p>Activities</p>}
-        </>
-        {props.currentUserID === props.chairID && <button>Add activity</button>}
-        <></>
+        {/* <> Only shows activities Header if there are activities */}
+        {(props.currentUserID === props.chairID ||
+          props.activities.length !== 0) && <p>Activities</p>}
       </div>
-      {props.activities.length !== 0 && activitylist}
+      <div className="container">
+        {props.activities.length !== 0 && activitylist}
+      </div>
+
+      {props.currentUserID === props.chairID && <button>Add activity</button>}
     </>
   );
 }
