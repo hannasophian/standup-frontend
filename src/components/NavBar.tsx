6805194b0a87c8a574d7da37c7c2cad9dae89579
Hanna <sophianhanna@gmail.com>
2022-01-31 13:24:43 +0000

    added logo and links to home

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,30 @@
 import PageProps from "../utils/interfaces/PageProps";
+import "../css/navbar.css";
+import { Link } from "react-router-dom";
 
 export default function NavBar(props: PageProps): JSX.Element {
   console.log(props.currentUser.image_url);
   return (
     <div className="navbar">
-      <img
-        className="user-photo"
-        src={
-          props.currentUser.image_url
-            ? props.currentUser.image_url
-            : "images/user.png"
-        }
-        alt={props.currentUser.name}
-      />
-      <p>Hi, {props.currentUser.name}</p>
+      <Link to="/">
+        <h1 className="navbar-logo">StandUp</h1>
+      </Link>
+      <div className="user-info">
+        <div className="container">
+          <span className="d-inline-block">Hi, {props.currentUser.name}</span>
+          <span className="d-inline-block btn float-right">
+            <img
+              className="user-photo"
+              src={
+                props.currentUser.image_url
+                  ? props.currentUser.image_url
+                  : "images/user.png"
+              }
+              alt={props.currentUser.name}
+            />
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
