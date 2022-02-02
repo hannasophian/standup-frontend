1e103fe9cbb17550eafbce149e19d32c60f6b19d
fazaamal <fazasophian123@gmail.com>
2022-02-02 22:40:44 +0800

    Header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,17 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import FormInterface from "../utils/interfaces/FormInterface";
 
+const styles = {
+  header: {
+    paddingTop : "10px",
+    backgroundColor: "orange"
+  },
+
+  container: {
+    paddingTop : "15px"
+  }
+} as const;
+
 export default function Home(): JSX.Element {
   const [formInfo, setFormInfo] = useState<FormInterface>({
     section: "",
@@ -16,84 +27,93 @@ export default function Home(): JSX.Element {
 
   return (
     <div>
-      <h1>OWL 1.0</h1>
-      <button className="btn btn-primary">Register</button>
-      <form>
-        <label htmlFor="sectionInput">Section</label>
-        <input
-          id="sectionInput"
-          onChange={(e) =>
-            setFormInfo({ ...formInfo, section: e.target.value })
-          }
-        />
-        <br />
-        <label htmlFor="stationInput">Station</label>
-        <fieldset id="stationInput">
+
+      <div className="row align-items-center justify-content-center" style={styles.header}>
+        <h1 className="col-sm-9">OWL 1.0</h1>
+        <div className="col-sm-1">
+          <button className="btn btn-primary">Register</button>
+        </div>
+      </div>
+      
+      <div className="container" style={styles.container}>
+        <form>
+          <label htmlFor="sectionInput">Section</label>
           <input
-            id="1"
+            id="sectionInput"
             onChange={(e) =>
-              setFormInfo({
-                ...formInfo,
-                station: [e.target.value, formInfo.station[1]],
-              })
+              setFormInfo({ ...formInfo, section: e.target.value })
             }
           />
-          <p>to</p>
+          <br />
+          <label htmlFor="stationInput">Station</label>
+          <fieldset id="stationInput">
+            <input
+              id="1"
+              onChange={(e) =>
+                setFormInfo({
+                  ...formInfo,
+                  station: [e.target.value, formInfo.station[1]],
+                })
+              }
+            />
+            <p>to</p>
+            <input
+              id="2"
+              onChange={(e) =>
+                setFormInfo({
+                  ...formInfo,
+                  station: [formInfo.station[0], e.target.value],
+                })
+              }
+            />
+          </fieldset>
+
+          <label htmlFor="directionInput">Direction</label>
           <input
-            id="2"
+            id="directionInput"
             onChange={(e) =>
-              setFormInfo({
-                ...formInfo,
-                station: [formInfo.station[0], e.target.value],
-              })
+              setFormInfo({ ...formInfo, direction: e.target.value })
             }
           />
-        </fieldset>
 
-        <label htmlFor="directionInput">Direction</label>
-        <input
-          id="directionInput"
-          onChange={(e) =>
-            setFormInfo({ ...formInfo, direction: e.target.value })
-          }
-        />
+          <br />
 
-        <br />
+          <label htmlFor="laneInput">Lane</label>
+          <input
+            id="laneInput"
+            onChange={(e) =>
+              setFormInfo({ ...formInfo, lane: parseInt(e.target.value) })
+            }
+          />
+          <br />
+          <label htmlFor="dateInput">Date</label>
+          <input
+            id="dateInput"
+            onChange={(e) => setFormInfo({ ...formInfo, date: e.target.value })}
+          />
 
-        <label htmlFor="laneInput">Lane</label>
-        <input
-          id="laneInput"
-          onChange={(e) =>
-            setFormInfo({ ...formInfo, lane: parseInt(e.target.value) })
-          }
-        />
-        <br />
-        <label htmlFor="dateInput">Date</label>
-        <input
-          id="dateInput"
-          onChange={(e) => setFormInfo({ ...formInfo, date: e.target.value })}
-        />
+          <br />
 
-        <br />
+          <label htmlFor="surveyorInput">Name of surveyor</label>
+          <input
+            id="surveyorInput"
+            onChange={(e) =>
+              setFormInfo({ ...formInfo, surveyor_name: e.target.value })
+            }
+          />
+          <br />
 
-        <label htmlFor="surveyorInput">Name of surveyor</label>
-        <input
-          id="surveyorInput"
-          onChange={(e) =>
-            setFormInfo({ ...formInfo, surveyor_name: e.target.value })
-          }
-        />
-        <br />
+          <label htmlFor="fileInput">Input data</label>
+          <fieldset id="fileInput">
+            <button>Upload Here</button>
+            <button>Upload Here</button>
+          </fieldset>
+        </form>
+        <Link to="/dashboard">
+          <button onClick={() => console.log(formInfo)}>Process</button>
+        </Link>
 
-        <label htmlFor="fileInput">Input data</label>
-        <fieldset id="fileInput">
-          <button>Upload Here</button>
-          <button>Upload Here</button>
-        </fieldset>
-      </form>
-      <Link to="/dashboard">
-        <button onClick={() => console.log(formInfo)}>Process</button>
-      </Link>
+      </div>
     </div>
   );
 }
