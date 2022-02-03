2ebe3170713deb3af55d9016959c01a3974fd3ea
Hanna <sophianhanna@gmail.com>
2022-02-03 07:38:40 +0000

    Loading symbol and disabled log in button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,8 @@ import { UserInterface } from "../utils/interfaces/UserInterface";
 import PageProps from "../utils/interfaces/PageProps";
 import { Link } from "react-router-dom";
 import "../css/home.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { BallTriangle } from "react-loader-spinner";
 
 export default function Home(props: PageProps): JSX.Element {
   const [allUsers, setAllUsers] = useState<UserInterface[]>([]);
@@ -29,10 +31,14 @@ export default function Home(props: PageProps): JSX.Element {
   }
 
   function logIn(id: number) {
+    // if (props.currentUser.id === 0) {
+    //   window.alert("Select a user to log in")
+    // } else {
     const user = getUserByID(id);
     // console.log(user)
     props.setCurrentUser(user);
     props.setTeam(user.team_id);
+    // }
   }
 
   return (
@@ -45,25 +51,33 @@ export default function Home(props: PageProps): JSX.Element {
             alt="two people having a conversation"
           />
           <h1>StandUp</h1>
-          <select
-            className="login-dropdown"
-            name="login"
-            id="inner"
-            value={props.currentUser.id}
-            onChange={(e) => {
-              logIn(parseInt(e.target.value));
-            }}
-          >
-            <option value={0} disabled>
-              Choose name to log in
-            </option>
-            {usersList}
-          </select>
-          <Link to="/dashboard">
-            <button type="button" className="btn btn-primary">
-              Log In
-            </button>
-          </Link>
+          {allUsers.length !== 0 ? (
+            <select
+              className="login-dropdown"
+              name="login"
+              id="inner"
+              value={props.currentUser.id}
+              onChange={(e) => {
+                logIn(parseInt(e.target.value));
+              }}
+            >
+              {" "}
+              <option value={0} disabled>
+                Choose name to log in
+              </option>
+              {usersList}
+            </select>
+          ) : (
+            <BallTriangle height="70px" color="#fc4f4f" ariaLabel="loading" />
+          )}
+
+          {props.currentUser.id !== 0 && (
+            <Link to="/dashboard">
+              <button type="button" className="btn btn-primary">
+                Log In
+              </button>
+            </Link>
+          )}
         </div>
       </div>
     </div>
